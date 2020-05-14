fb8a1d3487cddf628b2c0d6cfce8023f4ab709bc
Alex Root Junior <jroot.junior@gmail.com>
2020-05-15 00:46:32 +0300

    Fixes 44: Hate windows

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 if not os.getenv("SKIP_CYTHON", None):
     try:
@@ -7,13 +8,18 @@ if not os.getenv("SKIP_CYTHON", None):
         cythonize = None
         ext_modules = None
     else:
+        include = ["magic_filter/*.py", "magic_filter/operations/*.py"]
+        exclude = []
+        if sys.platform == "win32":  # Bug: https://github.com/cython/cython/issues/2968
+            exclude.extend(["magic_filter/operations/__init__.py", "magic_filter/__init__.py"])
+
         compiler_directives = {}
         if os.getenv("CYTHON_TRACE", False):
             compiler_directives["linetrace"] = True
         os.environ["CFLAGS"] = "-O3"
         ext_modules = cythonize(
-            ["magic_filter/*.py", "magic_filter/operations/*.py"],
-            exclude=[],
+            include,
+            exclude=exclude,
             nthreads=int(os.getenv("CYTHON_NTHREADS", 0)),
             language_level=3,
             compiler_directives=compiler_directives,
