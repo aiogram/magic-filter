4f25a8ddc83ad087a96ba4dcc41707fcb66e30ef
Alex Root Junior <jroot.junior@gmail.com>
2020-05-16 01:43:05 +0300

    Bump version

--- magic_filter/__init__.py
+++ magic_filter/__init__.py
@@ -1,16 +1,6 @@
 from . import operations
 from .magic import F, MagicFilter
 
-__all__ = ["F", "MagicFilter", "operations", "compiled"]
+__all__ = ["F", "MagicFilter", "operations"]
 
-__version__ = "0.1.1"
-
-try:  # pragma: no cover
-    import cython
-except ImportError:
-    compiled: bool = False
-else:  # pragma: no cover
-    try:
-        compiled = cython.compiled
-    except AttributeError:
-        compiled = False
+__version__ = "0.1.2"
