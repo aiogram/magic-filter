8252a355aae0893a108365b3136690ccf9076193
Łukasz Tshipenchko <dev@zxc.sx>
2023-03-26 17:48:19 +0600

    Fix missing exception message test

--- tests/test_len.py
+++ tests/test_len.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from magic_filter import F
@@ -7,5 +9,8 @@ class TestLen:
     def test_has_no_len(self):
         # F object doesn't have len(). But F.len() can be used,
         # so it will raise error with suggestion to use F.len()
-        with pytest.raises(TypeError):
+        error_message = (
+            "Length can't be taken using len() function. Use MagicFilter.len() instead."
+        )
+        with pytest.raises(TypeError, match=re.escape(error_message)):
             len(F)
